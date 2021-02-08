037cb896eaf4cabc9aaf7b34a297388203bf51dc
Humberto Garcia <humbertonood@gmail.com>
2021-02-08 14:33:51 -0300

    Proyecto terminado 2

--- falabella.php
+++ falabella.php
@@ -1,30 +1,82 @@
-<?php
+		<?php
+		 
 
-for ($i=1;$i<=100;$i++){
-echo$i;
+			 function imprimir($i){
 
-    if ($i%3==0 || $i%5==0 || $i%3==0 && $i%5==0){
 
-	    switch ($i) {
+					switch ($i) {
 
-        case ($i%3==0 && $i%5==0):
-			echo 'integraciones';
-			break;
+		                case ($i%3==0 && $i%5==0):
+					    return  'Integraciones';
+					    break;
+					
+				        case ($i%3==0):
+					    return  'Falabella';
+					    break;
+				
+				        case ($i%5==0):
+					    return 'IT';
+					    break;
+
+					}
+
+			}
+
+		
+
+		    function programa(){
+
+
+					for ($i=1;$i<=100;$i++){
+
+						$programa[]=array('Numero'=> $i,'Asignacion'=> imprimir($i)) ;
+
+					}
+
+					return $programa;
+
+			}
+
+
+
+			if (programa()<>NULL){
+				print_r(programa());
+			}
 			
-		case ($i%3==0):
-			echo 'Falabella';
-			break;
-		
-		case ($i%5==0):
-			echo 'IT';
-			break;
+
+
+
+
+
 
 		
 
-	    }
-	
-    }
 
-}
+	    /*for ($i=1;$i<=100;$i++){      /// LOGICA DEL EJERCICIO
+		echo$i;
 
-?>
+		    if ($i%3==0 || $i%5==0 || $i%3==0 && $i%5==0){
+
+			    switch ($i) {
+
+		        case ($i%3==0 && $i%5==0):
+					echo 'Integraciones';
+					break;
+					
+				case ($i%3==0):
+					echo 'Falabella';
+					break;
+				
+				case ($i%5==0):
+					echo 'IT';
+					break;
+
+				
+
+			    }
+
+		    }
+
+		}*/
+
+		?>
